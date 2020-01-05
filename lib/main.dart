b06e183ebf13f3ef7fe15845659b8a76b96024de
Harsh Mehta <deadspaceharsh@gmail.com>
2020-01-05 15:19:00 +0530

    merging

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 import 'package:poolite_sample/mainscreen.dart';
 
 void main() => runApp(MyApp());
@@ -12,19 +10,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static const LatLng _center = const LatLng(45.521563, -122.677433);
-  var location = new Location();
-  var currentLocation = LocationData;
-
-  void _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
-  get_ocation() async{
-    currentLocation = (await location.getLocation()) as Type;
-  }
 
   @override
   Widget build(BuildContext context) {
